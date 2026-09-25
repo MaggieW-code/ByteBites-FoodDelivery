03e2f664d32040b52ddbd6cf43ba0381a8de8dfb
maisonjar-dev <maison.personal03@gmail.com>
2026-09-24 21:12:32 -0700

    Aded pydantic M0-04 ticket model and testing

---
+++ backend/app/models/restaurant_model.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+
+
+class  Restaurant(BaseModel):
+    id: str = Field(strict=True, min_length=8, max_length=8)
+    name: str = Field(strict=True, min_length=1)
+    cuisine: str = Field(strict=True, min_length=1)
+    rating: float = Field(strict=True, ge=0, le=5)
+    address: str = Field(strict=True, min_length=1)
+    active: bool = Field(strict=True)
